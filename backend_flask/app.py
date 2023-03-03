d39da72be0c9c7d977073dc8441655baaa5895cb
Armando Diaz Gonzalez <arm.dan.diaz.gonzalez@gmail.com>
2023-03-02 22:01:08 -0500

    Add rollbar

--- backend_flask/app.py
+++ backend_flask/app.py
@@ -32,41 +32,55 @@ import watchtower
 import logging
 from time import strftime
 
+# Rollbar
+import rollbar
+import rollbar.contrib.flask
+from flask import got_request_exception
+
 # Configuring Logger to Use CloudWatch
 LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(logging.DEBUG)
 console_handler = logging.StreamHandler()
 cw_handler = watchtower.CloudWatchLogHandler(log_group='cruddur')
 LOGGER.addHandler(console_handler)
-LOGGER.addHandler(cw_handler)
+
+if os.getenv("ENABLE_CLOUDWATCH_LOG"):
+    LOGGER.info("Cloudwatch logger enabled")
+    LOGGER.addHandler(cw_handler)
 
 # HoneyComb
 # Initialize tracing and an exporter that can send data to Honeycomb
-provider = TracerProvider()
-processor = BatchSpanProcessor(OTLPSpanExporter())
-provider.add_span_processor(processor)
-
-# X Ray
-xray_url = os.getenv("AWS_XRAY_URL")
-xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
+if os.getenv("ENABLE_HONEYCOMB_LOG"):
+    LOGGER.info("Honeycomb logger enabled")
+    provider = TracerProvider()
+    processor = BatchSpanProcessor(OTLPSpanExporter())
+    provider.add_span_processor(processor)
+    trace.set_tracer_provider(provider)
+    tracer = trace.get_tracer(__name__)
 
 # Show this in the logs within the backend flask app (STDOUT)
-simple_processor = SimpleSpanProcessor(ConsoleSpanExporter())
-provider.add_span_processor(simple_processor)
+#simple_processor = SimpleSpanProcessor(ConsoleSpanExporter())
+#provider.add_span_processor(simple_processor)
 
-trace.set_tracer_provider(provider)
-tracer = trace.get_tracer(__name__)
+if os.getenv("ENABLE_XRAY_LOG"):
+    LOGGER.info("X-Ray logger enabled")
+    xray_url = os.getenv("AWS_XRAY_URL")
+    xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
 
 app = Flask(__name__)
 
 # X Ray
-XRayMiddleware(app, xray_recorder)
+if os.getenv("ENABLE_XRAY_LOG"):
+    XRayMiddleware(app, xray_recorder)
 
 # HoneyComb
 # Initialize automatic instrumentation with Flask
 FlaskInstrumentor().instrument_app(app)
 RequestsInstrumentor().instrument()
 
+# Rollbar
+rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+# Other env variables
 frontend = os.getenv("FRONTEND_URL")
 backend = os.getenv("BACKEND_URL")
 origins = [frontend, backend]
@@ -78,6 +92,23 @@ cors = CORS(
     methods="OPTIONS,GET,HEAD,POST",
 )
 
+@app.before_first_request
+@cross_origin()
+def init_rollbar():
+    """init rollbar module"""
+    if os.getenv("ENABLE_ROLLBAR_LOG"):
+        rollbar.init(
+            # access token
+            rollbar_access_token,
+            # environment name
+            'production',
+            # server root directory, makes tracebacks prettier
+            root=os.path.dirname(os.path.realpath(__file__)),
+            # flask already sets up logging
+            allow_logging_basic_config=False)
+
+        # send exceptions from `app` to rollbar, using flask's signal system.
+        got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
 
 @app.after_request
 @cross_origin()
@@ -85,6 +116,15 @@ def after_request(response):
     timestamp = strftime('[%Y-%b-%d %H:%M]')
     LOGGER.error('%s %s %s %s %s %s', timestamp, request.remote_addr, request.method, request.scheme, request.full_path, response.status)
     return response
+
+@app.route('/rollbar/test')
+@cross_origin()
+def rollbar_test():
+    if os.getenv("ENABLE_ROLLBAR_LOG"):
+        rollbar.report_message('Hello World!', 'warning')
+        return "Rollbar: Hello World!"
+    else:
+        return "Hello World!"
 
 @app.route("/api/message_groups", methods=["GET"])
 @cross_origin()
@@ -134,6 +174,7 @@ def data_create_message():
 
 @app.route("/api/activities/home", methods=["GET"])
 @cross_origin()
+# @xray_recorder.capture("home-activities")
 def data_home():
     data = HomeActivities.run(logger=LOGGER)
     return data, 200
--- backend_flask/services/home_activities.py
+++ backend_flask/services/home_activities.py
@@ -1,57 +1,105 @@
 from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
+from aws_xray_sdk.core import xray_recorder
+import os
 
-tracer = trace.get_tracer("home.activities")
+if os.getenv("ENABLE_HONEYCOMB_LOG"):
+    tracer = trace.get_tracer("home.activities")
 
 
 class HomeActivities:
-    def run(logger):
-        #logger.info("Home Activities")
-        with tracer.start_as_current_span("home-activities-mock-data"):
-            span = trace.get_current_span()
-            now = datetime.now(timezone.utc).astimezone()
-            span.set_attribute("app.now", now.isoformat())
-            results = [
-                {
-                    "uuid": "68f126b0-1ceb-4a33-88be-d90fa7109eee",
-                    "handle": "Armando Diaz",
-                    "message": "Cloud is fun!",
-                    "created_at": (now - timedelta(days=2)).isoformat(),
-                    "expires_at": (now + timedelta(days=5)).isoformat(),
-                    "likes_count": 5,
-                    "replies_count": 1,
-                    "reposts_count": 0,
-                    "replies": [
-                        {
-                            "uuid": "26e12864-1c26-5c3a-9658-97a10f8fea67",
-                            "reply_to_activity_uuid": "68f126b0-1ceb-4a33-88be-d90fa7109eee",
-                            "handle": "Worf",
-                            "message": "This post has no honor!",
-                            "likes_count": 0,
-                            "replies_count": 0,
-                            "reposts_count": 0,
-                            "created_at": (now - timedelta(days=2)).isoformat(),
-                        }
-                    ],
-                },
-                {
-                    "uuid": "66e12864-8c26-4c3a-9658-95a10f8fea67",
-                    "handle": "Worf",
-                    "message": "I am out of prune juice",
-                    "created_at": (now - timedelta(days=7)).isoformat(),
-                    "expires_at": (now + timedelta(days=9)).isoformat(),
-                    "likes": 0,
-                    "replies": [],
-                },
-                {
-                    "uuid": "248959df-3079-4947-b847-9e0892d1bab4",
-                    "handle": "Garek",
-                    "message": "My dear doctor, I am just simple tailor",
-                    "created_at": (now - timedelta(hours=1)).isoformat(),
-                    "expires_at": (now + timedelta(hours=12)).isoformat(),
-                    "likes": 0,
-                    "replies": [],
-                },
-            ]
-            span.set_attribute("app.result_length", len(results))
+    @staticmethod
+    def get_data(now):
+        results = [
+            {
+                "uuid": "68f126b0-1ceb-4a33-88be-d90fa7109eee",
+                "handle": "Armando Diaz",
+                "message": "Cloud is fun!",
+                "created_at": (now - timedelta(days=2)).isoformat(),
+                "expires_at": (now + timedelta(days=5)).isoformat(),
+                "likes_count": 5,
+                "replies_count": 1,
+                "reposts_count": 0,
+                "replies": [
+                    {
+                        "uuid": "26e12864-1c26-5c3a-9658-97a10f8fea67",
+                        "reply_to_activity_uuid": "68f126b0-1ceb-4a33-88be-d90fa7109eee",
+                        "handle": "Worf",
+                        "message": "This post has no honor!",
+                        "likes_count": 0,
+                        "replies_count": 0,
+                        "reposts_count": 0,
+                        "created_at": (now - timedelta(days=2)).isoformat(),
+                    }
+                ],
+            },
+            {
+                "uuid": "66e12864-8c26-4c3a-9658-95a10f8fea67",
+                "handle": "Worf",
+                "message": "I am out of prune juice",
+                "created_at": (now - timedelta(days=7)).isoformat(),
+                "expires_at": (now + timedelta(days=9)).isoformat(),
+                "likes": 0,
+                "replies": [],
+            },
+            {
+                "uuid": "248959df-3079-4947-b847-9e0892d1bab4",
+                "handle": "Garek",
+                "message": "My dear doctor, I am just simple tailor",
+                "created_at": (now - timedelta(hours=1)).isoformat(),
+                "expires_at": (now + timedelta(hours=12)).isoformat(),
+                "likes": 0,
+                "replies": [],
+            },
+        ]
+        return results
+
+    def run(self, logger):
+        logger.info("Home Activities")
+        now = datetime.now(timezone.utc).astimezone()
+        if os.getenv("ENABLE_HONEYCOMB_LOG"):
+            with tracer.start_as_current_span("home-activities-mock-data"):
+                span = trace.get_current_span()
+                span.set_attribute("app.now", now.isoformat())
+                results = self.get_data()
+                span.set_attribute("app.result_length", len(results))
+                return results
+        elif os.getenv("ENABLE_CLOUDWATCH_LOG"):
+            logger.info("home-activities-mock-data")
+            logger.info(f"app.now: {now.isoformat()}")
+            results = self.get_data()
+            logger.info(f"app.result_length: {len(results)}")
             return results
+        elif os.getenv("ENABLE_XRAY_LOG"):
+            segment = xray_recorder.begin_segment('home-activities')
+            xray_time_dict = {
+                "now": now.isoformat()
+            }
+
+            segment.put_metadata('now', xray_time_dict, 'home-activities-now')
+
+            subsegment = xray_recorder.begin_subsegment('home-activities-mock-data')
+
+            results = self.get_data()
+
+            xray_results_size_dict = {
+               "result-size": len(results)
+            }
+
+            subsegment.put_annotation('result-size', xray_results_size_dict, 'home-activities-mock-data-results-size')
+
+            xray_recorder.end_subsegment()
+            xray_recorder.end_segment()
+
+            return results
+        else:
+            logger.info("No loggers are running")
+            return results
+
+class A(object):
+    @staticmethod
+    def stat_meth():
+        print("Look no self was passed")
+    
+    def run(self):
+        self.stat_meth()
