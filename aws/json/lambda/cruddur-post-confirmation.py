cca6d9f8c3be63f68fbc6c09fc610b5f48b0c68e
Armando Diaz Gonzalez <arm.dan.diaz.gonzalez@gmail.com>
2023-03-13 19:46:00 -0400

    Add db and bin files for postgres connection

---
+++ aws/json/lambda/cruddur-post-confirmation.py
@@ -0,0 +1,26 @@
+import os
+import json
+import psycopg2
+
+def lambda_handler(event, context):
+    user = event['request']['userAttributes']
+    try:
+        conn = psycopg2.connect(os.getenv('CONNECTION_URL'))
+        cur = conn.cursor()
+        user_display_name = user['name']
+        user_email = user['email']
+        user_handle = user['preferred_username']
+        user_cognito_id = user['sub']
+        cur.execute(f"INSERT INTO users (display_name, email, handle, cognito_user_id) VALUES({user_display_name}, {user_email}, {user_handle}, {user_cognito_id})")
+        conn.commit() 
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+        
+    finally:
+        if conn is not None:
+            cur.close()
+            conn.close()
+            print('Database connection closed.')
+
+    return event
