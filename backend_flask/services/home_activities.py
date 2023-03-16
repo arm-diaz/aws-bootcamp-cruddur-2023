1282f2e1ec24d6d2678067578cffca1c5d3beaa1
Armando Diaz Gonzalez <arm.dan.diaz.gonzalez@gmail.com>
2023-03-16 16:22:14 -0400

    week4: create activities

--- backend_flask/services/home_activities.py
+++ backend_flask/services/home_activities.py
@@ -2,7 +2,7 @@ from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
 import os
 
-from libs.db import pool, query_wrap_array
+from libs.db import db
 
 if os.getenv("ENABLE_HONEYCOMB_LOG"):
     tracer = trace.get_tracer("home.activities")
@@ -12,34 +12,11 @@ class HomeActivities(object):
     
     @staticmethod
     def get_data(now, cognito_user_id):
-        sql = query_wrap_array("""
-                                SELECT
-                                    activities.uuid,
-                                    users.display_name,
-                                    users.handle,
-                                    activities.message,
-                                    activities.replies_count,
-                                    activities.reposts_count,
-                                    activities.likes_count,
-                                    activities.reply_to_activity_uuid,
-                                    activities.expires_at,
-                                    activities.created_at
-                                FROM public.activities
-                                LEFT JOIN public.users ON users.uuid = activities.user_uuid
-                                ORDER BY activities.created_at DESC
-        """)
-        print(sql)
+        sql = db.template('activities','home')
 
-        with pool.connection() as conn:
-            with conn.cursor() as cur:
-                cur.execute(sql)
-                # this will return a tuple
-                # the first field being the data
-                results = cur.fetchall()
+        results = db.query_array_json(sql)
 
-        print(results)
-
-        if cognito_user_id != None:
+        if cognito_user_id == None:
             extra_crud = {
                 "uuid": "248959df-3079-4947-b847-9e0892d1bab4",
                 "handle": "Lore",
